b35480d754778bf84a59d68ab98d5ef96327f8d4
Marcos <nodmarcos@gmail.com>
2022-02-24 23:59:35 -0300

    consumed an API with view/controller, installed and used Redis to make cache of the questions, added an editor WYSIWYG and installed and configured elasticsearch on rails, course finished

---
+++ time_to_answer/app/helpers/site_helper.rb
@@ -0,0 +1,12 @@
+module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+      when 'index'
+        "Ultimas perguntas cadastradas..."
+      when 'questions'
+        "Resultados para o termo \"#{sanitize params[:term]}\"..."
+      when 'subject'
+        "Mostrando questões para o assunto \"#{sanitize params[:subject]}\"..."
+    end
+  end
+end
